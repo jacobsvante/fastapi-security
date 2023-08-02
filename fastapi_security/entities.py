53108277652eed08704fee8f2997d58efdc2458d
chrisladden <cladden@om1.com>
2023-08-02 09:22:34 -0400

    remove unused import

--- fastapi_security/entities.py
+++ fastapi_security/entities.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from enum import Enum
 from typing import Any, Dict, List, Optional, Annotated
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field
 from pydantic.functional_validators import BeforeValidator
 
 
